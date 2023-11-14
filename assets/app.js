7ef9139dd80220e2e517e8b9d93f9a451a49e046
Oli <oifranco83@gmail.com>
2023-11-14 15:22:51 -0300

    aplicando estilo

---
+++ assets/app.js
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8"/>
+        <title>PokeApi Ejerc4</title>
+        <link rel="stylesheet" href="./assets/estilos.css"></link>
+        
+    </head>
+    <body>
+    <div class="Container"></div>	
+	        <form action="" class="form">
+            <input type="number" name="" id="selector">
+            <span id="idContainer"></span>
+            <div class="wrapper">
+                <button class="caja" type="sub	mit">
+                    <span>SELECCIONAR</span>
+                </button>
+            </div>
+           
+        </form>
+        <script src="./assets/app.js"></script>
+        
+        
+    </body>
+</html>
