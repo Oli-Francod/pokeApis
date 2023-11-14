65d570f573d694a6f59de67f6410ff7b93b38743
Oli <oifranco83@gmail.com>
2023-11-14 15:46:56 -0300

    carga final

--- assets/app.js
+++ assets/app.js
@@ -1,25 +1,87 @@
-<!doctype html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8"/>
-        <title>PokeApi Ejerc4</title>
-        <link rel="stylesheet" href="./assets/estilos.css"></link>
-        
-    </head>
-    <body>
-    <div class="Container"></div>	
-	        <form action="" class="form">
-            <input type="number" name="" id="selector">
-            <span id="idContainer"></span>
-            <div class="wrapper">
-                <button class="caja" type="sub	mit">
-                    <span>SELECCIONAR</span>
-                </button>
-            </div>
-           
-        </form>
-        <script src="./assets/app.js"></script>
-        
-        
-    </body>
-</html>
+document.getElementById("idContainer");
+const contenedor = document.querySelector(".Container")
+  , errNum = async nombre=>{
+    nombre.preventDefault();
+    let pokmon = selector.value;
+    contenedor.innerHTML = "";
+    const renderFound = "<span>ERROR: Verifique el N° agregado</span>";
+    if (!vacio(pokmon))
+        return contenedor.innerHTML = renderFound;
+    const tipo = await rend(pokmon);
+    if (tipo == null || tipo == null)
+        return contenedor.innerHTML = renderFound;
+    pokeSize(tipo)
+}, insertNum = ()=>{
+    form.addEventListener("submit", errNum )
+};
+
+
+const vacio = nombre=>{
+    if (nombre === "") {
+     idContainer.innerHTML = "ERROR: Agregue un numero";
+     return
+ }
+    return idContainer.innerHTML = "",!0
+}
+, rend = async nombre=>{
+ const pokmon = "https://pokeapi.co/api/v2/pokemon/";
+ try {
+     return await (await fetch(pokmon + nombre)).json()
+ } catch {
+     idContainer.innerHTML = `ERROR: El pokemon ${nombre} no existe`;
+     return
+ }
+}
+, take = nombre=>{
+ let pokmon = [];
+ return nombre.forEach(renderFound=>{
+     pokmon.push(renderFound.type.name)}), pokmon}
+
+,  size = nombre=>Math.ceil(nombre * .3048)
+,  pokeSize = ({name: nombre, weight: pokmon, sprites: renderFound, types: tipo, height: alt})=>contenedor.innerHTML = `
+                        <div class="card">
+                        <h2>${nombre.toUpperCase()}</h2>
+                        <img src="${renderFound.other.dream_world.front_default || renderFound.front_default}" alt="">
+                        <div class="ClassContainer">
+                        <p>Tipos:</p>
+                        ${take(tipo).join(", ")}
+                        </div>
+                        <p>Altura = ${size(alt)} mts</p>
+                        <p>Peso = ${pokmon} kg</p>
+                        </div>`
+, form = document.querySelector(".form")
+, selector = document.getElementById("selector");
+
+(function() {
+    const pokmon = document.createElement("link").relList;
+    if (pokmon && pokmon.supports && pokmon.supports("modulepreload"))
+        return;
+    for (const alt of document.querySelectorAll('link[rel="modulepreload"]'))
+        tipo(alt);
+    new MutationObserver(e=>{
+        for (const inicio of alt)
+            if (inicio.type === "childList")
+                for (const carga of inicio.addedNodes)
+                    carga.tagName === "LINK" && carga.rel === "modulepreload" && tipo(carga)
+    }
+    ).observe(document, {
+        childList: !0,
+        subtree: !0
+    });
+    function renderFound(e) {
+        const inicio = {};
+        return alt.integrity && (inicio.integrity = alt.integrity),
+        alt.referrerPolicy && (inicio.referrerPolicy = alt.referrerPolicy),
+        alt.crossOrigin === "use-credentials" ? inicio.credentials = "include" : alt.crossOrigin === "anonymous" ? inicio.credentials = "omit" : inicio.credentials = "same-origin",
+        inicio
+    }
+    function tipo(e) {
+        if (alt.ep)
+            return;
+        alt.ep = !0;
+        const inicio = renderFound(alt);
+        fetch(alt.href, inicio)
+    }
+}
+)();
+insertNum();
